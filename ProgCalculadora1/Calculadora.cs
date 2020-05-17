91ec68051adbb901d60399201be50e8fc775228d
EuJunqueira <filipe.junqueira@alunos.unis.edu.br>
2020-05-17 18:22:27 -0300

    first commit

---
+++ ProgCalculadora1/Calculadora.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgCalculadora1
+{
+    class Calculadora
+    {
+        public double Numero;
+        public int Referencia;
+
+        private List<double> listNumero = new List<double>();
+        public void addNumero(double Numero)
+        {
+            this.listNumero.Add(Numero);
+        }
+        public double Somar()
+        {
+            double Resultado = 0;
+            foreach (double Numero in this.listNumero)
+            {
+                Resultado += Numero;
+            }
+            this.listNumero.Clear();
+            return Resultado;
+        }
+        public double Subtrair()
+        {
+            double Resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double Numero in this.listNumero)
+            {
+                Resultado -= Numero;
+            }
+            this.listNumero.Clear();
+            return Resultado;
+        }
+        public double Multiplicar()
+        {
+            double Resultado = 1;
+            foreach (double Numero in this.listNumero)
+            {
+                Resultado *= Numero;
+            }
+            this.listNumero.Clear();
+            return Resultado;
+        }
+        public double Dividir()
+        {
+            double Resultado = 0;
+            foreach (double Numero in this.listNumero)
+            {
+                Resultado /= Numero;
+            }
+            this.listNumero.Clear();
+            return Resultado;
+        }
+        public double Cubo()
+        {
+            double Resultado = 1;
+            foreach (double Numero in this.listNumero)
+            {
+                Resultado = Numero * Numero * Numero;
+            }
+            this.listNumero.Clear();
+            return Resultado;
+        }
+    }
+}
---
+++ ProgCalculadora1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ProgCalculadora1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Calculadora objCalculadora = new Calculadora();
+            System.Console.Write("\n\n    MENU\n    01 - Realizar Adição\n    02 - Realizar Subtração\n    03 - Realizar Multiplicação\n    04 - Realizar Divisão\n    05 - Elevar ao Cubo\n    >: ");
+
+            objCalculadora.Numero = (Convert.ToInt32(System.Console.ReadLine()));
+            if (objCalculadora.Numero >= 1 && objCalculadora.Numero <= 5)
+            {
+                System.Console.Write("\n\n    Quantos números deseja trabalhar? Digite aqui: ");
+                objCalculadora.Referencia = (Convert.ToInt32(System.Console.ReadLine()));
+                System.Console.Write("\n");
+                if (objCalculadora.Numero == 1)
+                {
+                    for (int i = 0; i < objCalculadora.Referencia; i++)
+                    {
+                        Console.Write("    Digite um número aqui para realizar a adição: "); objCalculadora.addNumero(Convert.ToDouble(System.Console.ReadLine()));
+                    }
+                    Console.Write("\n    Resultado = " + objCalculadora.Somar());
+                }
+                else if (objCalculadora.Numero == 2)
+                {
+                    for (int i = 0; i < objCalculadora.Referencia; i++)
+                    {
+                        Console.Write("    Digite um número aqui para realizar a subtração: ");
+                        objCalculadora.addNumero(Convert.ToDouble(System.Console.ReadLine()));
+                    }
+                    Console.Write("\n    Resultado = " + objCalculadora.Subtrair());
+                }
+                else if (objCalculadora.Numero == 3)
+                {
+                    for (int i = 0; i < objCalculadora.Referencia; i++)
+                    {
+                        Console.Write("    Digite um número aqui para realizar a multiplicação: ");
+                        objCalculadora.addNumero(Convert.ToDouble(System.Console.ReadLine()));
+                    }
+                    Console.Write("\n    Resultado = " + objCalculadora.Multiplicar());
+                }
+                else if (objCalculadora.Numero == 4)
+                {
+                    for (int i = 0; i < objCalculadora.Referencia; i++)
+                    {
+                        Console.Write("    Digite um número aqui para realizar a divisão: ");
+                        objCalculadora.addNumero(Convert.ToDouble(System.Console.ReadLine()));
+                    }
+                    Console.Write("\n    Resultado = " + objCalculadora.Dividir());
+                }
+                else if (objCalculadora.Numero == 5)
+                {
+                    Console.Write("\n    Digite um número aqui para elevá-lo ao cubo: ");
+                    objCalculadora.addNumero(Convert.ToDouble(System.Console.ReadLine()));
+                    Console.Write("\n    Resultado = " + objCalculadora.Cubo());
+                }
+                System.Console.ReadKey();
+            }
+
+        }
+    }
+}
